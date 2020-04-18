5d4ac71711421aae6befccfcb7e7e87c50236053
Cameron Borthwick <camee14@hotmail.co.uk>
2020-04-18 10:32:31 +0100

    init project setup

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour
+{
+    private Game m_gameInstance;
+
+    void Awake()
+    {
+        m_gameInstance = new Game();
+    }
+
+    void Update()
+    {
+        m_gameInstance.Update();
+    }
+}
